50edd927060db2d11e9e5d446f0728f95ad5ec0c
Austin Limanek <austin.limanek@gmail.com>
2022-05-25 23:32:28 -0700

    added several functions and modified the text in several sections. I also modified the alignment of several tags

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 
 function myFunc(){
-    alert("You Clicked me");
+    alert("You are a true scientist. Keep experimenting!");
 }
 
 function hilig(){
-    let scientist = "true"
+    alert("I couldn't agree more!")
+}
+
+function submit(){
+    if (document.getElementById('scisub').value == "physics") {
+        alert("I agree, this is my absolute favorite subject");
+    }
+    else if (document.getElementById('scisub').value == "chemistry"){
+        alert("Due to the property of spin, particles take up non-zero volume");
+    }
+    else if (document.getElementById('scisub').value == "biology"){
+        alert("Vaccines are synthesized by deeply understanding both biology and chemistry");
+    }
+    else if (document.getElementById('scisub').value == "geology"){
+        alert("Sometimes confused with geography, but the two go hand in hand due to mountain ranges and rivers often defining the borders between countries");
+    }
+    else if (document.getElementById('scisub').value == ""){
+        alert("You need to type something into the box before submitting")
+    }
+    else {
+        alert("I don't believe that was one of the choices?");
+    }
 }
